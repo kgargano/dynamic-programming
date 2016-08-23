532167714636557ca0f03538fe225f2412394879
Keith Gargano <keith.gargano@versionone.com>
2016-08-22 21:26:20 -0400

    set up failing tests

--- js/src/largestSumContiguousSequence.spec.js
+++ js/src/largestSumContiguousSequence.spec.js
@@ -6,3 +6,30 @@ describe('when running a sequence of one element', () => {
         largestSumContiguousSequence(sequence).should.equal(1);
     });
 });
+
+
+describe('when running a sequence [-1, -10, 10, 2]', () => {
+    let sequence = [-1, -10, 10, 2]
+    it('it should return 12 as the max', () => {
+        largestSumContiguousSequence(sequence).should.equal(12);
+    });
+});
+
+
+describe('when running a sequence [-1, -10, 10, 2, -13, 14]', () => {
+    let sequence = [-1, -10, 10, 2, -13, 14]
+    it('it should return 14 as the max', () => {
+        largestSumContiguousSequence(sequence).should.equal(14);
+    });
+});
+
+
+describe('when running a sequence [-1, -10, 10, 2, -13, 14, 1]', () => {
+    let sequence = [-1, -10, 10, 2, -13, 14, 1]
+    it('it should return 15 as the max', () => {
+        largestSumContiguousSequence(sequence).should.equal(15);
+    });
+});
+
+
+

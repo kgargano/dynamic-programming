83bf551bc43da85ca8a723b103a0a076cd08dd43
Keith Gargano <keith.gargano@versionone.com>
2016-08-25 09:25:58 -0400

    redefine tests for empty subsequence

--- js/src/longestIncreasingSubsequence.spec.js
+++ js/src/longestIncreasingSubsequence.spec.js
@@ -2,15 +2,22 @@ import longestIncreasingSubsequence from './longestIncreasingSubsequence'
 
 describe('when running a sequence of one element', () => {
     let sequence = [1];
-    it('it should return that element as longest increasing subsequence', () => {
-        longestIncreasingSubsequence(sequence).should.deep.equal([1]);
+    it('it should return an empty subsequence', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([]);
+    });
+});
+
+describe('when running a sequence [-1, -2, -3]', () => {
+    let sequence = [-1, -2, -3];
+    it('it should return []', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([]);
     });
 });
 
 describe('when running a sequence [1, -1, 2, 3]', () => {
     let sequence = [1, -1, 2, 3];
-    it('it should return [2, 3]', () => {
-        longestIncreasingSubsequence(sequence).should.deep.equal([2, 3]);
+    it('it should return [-1, 2, 3]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([-1, 2, 3]);
     });
 });
 

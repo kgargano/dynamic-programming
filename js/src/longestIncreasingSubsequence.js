92e207155528ef8a17b17db3458f90c86ba5fab2
Keith Gargano <keith.gargano@versionone.com>
2016-08-25 09:25:58 -0400

    first pass at implementation, doesn't handle non-contiguous and fails at subsequence with a last element in subsequence

--- js/src/longestIncreasingSubsequence.js
+++ js/src/longestIncreasingSubsequence.js
@@ -1,3 +1,23 @@
 export default (sequence)=> {
-    return sequence;
+
+    let currentSubsequence = [],
+        longestSubsequence = [];
+
+    sequence.forEach((element)=>{
+        if(currentSubsequence.length == 0){
+            currentSubsequence.push(element);
+        }
+
+        if(longestSubsequence.length < currentSubsequence.length){
+            longestSubsequence = currentSubsequence;
+        }
+
+        if(currentSubsequence[currentSubsequence.length-1] < element){
+            currentSubsequence.push(element);
+        }else{
+            currentSubsequence = [element];
+        }
+    });
+
+    return longestSubsequence;
 };

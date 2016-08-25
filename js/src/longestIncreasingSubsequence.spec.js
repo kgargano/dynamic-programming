0e8f8cb49ecdabe629acd461bcf654b3c47eaabe
Keith Gargano <keith.gargano@versionone.com>
2016-08-24 21:32:28 -0400

    stub out tests for longest increasing subsequence

--- js/src/longestIncreasingSubsequence.spec.js
+++ js/src/longestIncreasingSubsequence.spec.js
@@ -1,8 +1,43 @@
 import longestIncreasingSubsequence from './longestIncreasingSubsequence'
 
 describe('when running a sequence of one element', () => {
-    let sequence = [1]
+    let sequence = [1];
     it('it should return that element as longest increasing subsequence', () => {
         longestIncreasingSubsequence(sequence).should.deep.equal([1]);
+    });
+});
+
+describe('when running a sequence [1, -1, 2, 3]', () => {
+    let sequence = [1, -1, 2, 3];
+    it('it should return [2, 3]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([2, 3]);
+    });
+});
+
+describe('when running a sequence [1, 2, 3]', () => {
+    let sequence = [1, 2, 3];
+    it('it should return [1, 2, 3]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([1, 2, 3]);
+    });
+});
+
+describe('when running a sequence [1, 2, 3, -3, 3, 2, 1, 2, 3]', () => {
+    let sequence = [1, 2, 3, -3, 3, 2, 1, 2, 3];
+    it('it should return [1, 2, 3]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([1, 2, 3]);
+    });
+});
+
+describe('when running a sequence [1, 2, 3, -3, 3, 2, 1, 2, 3, 4]', () => {
+    let sequence = [1, 2, 3, -3, 3, 2, 1, 2, 3, 4];
+    it('it should return [1, 2, 3, 4]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([1, 2, 3, 4]);
+    });
+});
+
+describe('when running a sequence [-1, -2, -1, 0, 1]', () => {
+    let sequence = [-1, -2, -1, 0, 1];
+    it('it should return [-2, -1, 0, 1]', () => {
+        longestIncreasingSubsequence(sequence).should.deep.equal([-2, -1, 0, 1]);
     });
 });

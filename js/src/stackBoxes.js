4c97bc1af0ae5285e8e54d0935cb94888eb00d41
Keith Gargano <Keith.Gargano@gmail.com>
2016-09-01 18:19:35 -0400

    WIP.. thinking through the problem.. should probably write with tests

--- js/src/stackBoxes.js
+++ js/src/stackBoxes.js
@@ -1,7 +1,31 @@
 
 
-
 export default (boxes)=>{
+
+
+
+    const boxesSortedByArea = boxes.map((box)=>[
+            {l:box.a, w:box.b, h:box.c, base: box.a*box.b},
+            {l:box.b, w:box.c, h:box.a, base: box.b*box.c},
+            {l:box.c, w:box.a, h:box.b, base: box.c*box.a}
+            ].reduce((orientation, orientations)=>{
+                if(!orientations.find(o=> o.base == orientation.base)){
+                    orientations.push(orientation)
+                }
+            }, []))
+        .reduce((orientations, all)=>orientations.concat(all), [])
+        .sort((box1, box2)=> box2 - box1);
+
+    const boxesSortedByHeight = boxesSortedByArea.sort((b1, b2)=> b1.h - b2.h);
+
+
+    let stacks = [];
+
+
+    boxesSortedByArea.foreach((box)=>{
+
+    });
+
 
     let sides = []
     for(let side in boxes[0]){
--- js/src/stackBoxes.spec.js
+++ js/src/stackBoxes.spec.js
@@ -4,9 +4,20 @@ describe('when stacking one box', () => {
     let boxes = [{a: 4, b: 3, c: 2}]
     let stackedBoxes = stackBoxes(boxes);
     it('it should return one box', () => {
-        stackedBoxes.length.should.equal(1)
+        stackedBoxes.l.should.equal(1)
     });
     it('it should orient the box with height 4', () => {
-        stackedBoxes[0].height.should.equal(4)
+        stackedBoxes[0].h.should.equal(4)
+    });
+});
+
+
+describe('when stacking a box x:{a:1, b:1, c:1} and y:{a:2, b:2, c:2}', () => {
+
+    let boxes = [{a: 4, b: 3, c: 2}]
+    let stackedBoxes = stackBoxes(boxes);
+
+    it('should stack x on top of y', ()=>{
+
     });
 });

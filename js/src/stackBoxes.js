42955e32152ebc00795a90c5d4f4d126b01ee2a3
Keith Gargano <Keith.Gargano@gmail.com>
2016-09-01 17:19:45 -0400

    naive impl of first test

--- js/src/stackBoxes.js
+++ js/src/stackBoxes.js
@@ -1,3 +1,18 @@
+
+
+
 export default (boxes)=>{
-    return [];
+
+    let sides = []
+    for(let side in boxes[0]){
+        sides.push(boxes[0][side]);
+    }
+    sides.sort();
+
+
+    return [{
+        height: sides.pop(),
+        width: sides.pop(),
+        depth: sides.pop()
+    }];
 }

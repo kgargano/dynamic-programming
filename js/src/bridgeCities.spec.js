0bf66660683e1908f23119bdc441174000f46798
Keith Gargano <Keith.Gargano@gmail.com>
2016-09-01 21:49:27 -0400

    stub out tests describing data structure

--- js/src/bridgeCities.spec.js
+++ js/src/bridgeCities.spec.js
@@ -6,3 +6,24 @@ describe('when there are no cities to bridge', ()=>{
         bridgedCities.should.deep.equal([]);
     });
 });
+
+
+describe('when there is a single matching city on either side', ()=>{
+
+    let bridgedCities = bridgeCities([['a'],['a']]);
+    it('it returns a single connection between those cities', ()=>{
+        bridgedCities.length.should.equal(1);
+    });
+    it('it returns a connection for that pair with length 0', ()=>{
+        bridgedCities.should.deep.equal([{pair:'a', length:0}]);
+    });
+});
+
+describe('when there is are disjoint cities on either side', ()=>{
+
+    let bridgedCities = bridgeCities([['a'], ['b']]);
+    it('it returns an empty result set', ()=>{
+        bridgedCities.should.deep.equal([]);
+    });
+});
+

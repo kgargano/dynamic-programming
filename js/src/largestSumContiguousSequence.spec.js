e4ce9111beaac8d52f4298da9f8087f856e1797f
Keith Gargano <keith.gargano@gmail.com>
2016-09-09 14:25:16 -0400

    refactor summation tests to use a shared actual

--- js/src/largestSumContiguousSequence.spec.js
+++ js/src/largestSumContiguousSequence.spec.js
@@ -2,67 +2,76 @@ import largestSumContiguousSequence from './largestSumContiguousSequence'
 
 describe.only('largest sum contiguous sequence tests', ()=>{
     describe('when running a sequence of one element', () => {
-        let sequence = [1]
+        let sequence = [1],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return that element as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(1);
+            actual.max.should.equal(1);
         });
     });
 
     describe('when running a sequence [1, 2]', () => {
-        let sequence = [1, 2]
+        let sequence = [1, 2],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return 3 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(3);
+            actual.max.should.equal(3);
         });
     });
 
     describe('when running a sequence [-1, -10, 10, 2]', () => {
-        let sequence = [-1, -10, 10, 2]
+        let sequence = [-1, -10, 10, 2],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return 12 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(12);
+            actual.max.should.equal(12);
         });
     });
 
     describe('when running a sequence [-1, -10, 10, 2, -13, 14]', () => {
-        let sequence = [-1, -10, 10, 2, -13, 14]
+        let sequence = [-1, -10, 10, 2, -13, 14],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return 14 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(14);
+            actual.max.should.equal(14);
         });
     });
 
     describe('when running a sequence [-1, -10, 10, 2, -13, 14, 1]', () => {
-        let sequence = [-1, -10, 10, 2, -13, 14, 1]
+        let sequence = [-1, -10, 10, 2, -13, 14, 1],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return 15 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(15);
+            actual.max.should.equal(15);
         });
     });
 
     describe('when running a sequence [-1, -10, -3, -5]', () => {
-        let sequence = [-1, -10, -3, -5]
+        let sequence = [-1, -10, -3, -5],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return -1 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(-1);
+            actual.max.should.equal(-1);
         });
     });
 
 
     describe('when running a sequence [-3, -2, -1]', () => {
-        let sequence = [-3, -2, -1]
+        let sequence = [-3, -2, -1],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return -1 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(-1);
+            actual.max.should.equal(-1);
         });
     });
 
     describe('when running a sequence [-12, -10, -3, -5]', () => {
-        let sequence = [-12, -10, -3, -5]
+        let sequence = [-12, -10, -3, -5],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return -3 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(-3);
+            actual.max.should.equal(-3);
         });
     });
 
 
     describe('when running a sequence [8, 4, -1, 2]', () => {
-        let sequence = [8, 4, -1, 2]
+        let sequence = [8, 4, -1, 2],
+            actual = largestSumContiguousSequence(sequence);
         it('it should return 13 as the max', () => {
-            largestSumContiguousSequence(sequence).max.should.equal(13);
+            actual.max.should.equal(13);
         });
     });
 });

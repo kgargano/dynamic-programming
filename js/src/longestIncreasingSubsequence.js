b91dc48028fb9edf79b8af1106d9df747f862a90
Keith Gargano <keith.gargano@gmail.com>
2016-09-10 10:59:22 -0400

    LIS cleanup

--- js/src/longestIncreasingSubsequence.js
+++ js/src/longestIncreasingSubsequence.js
@@ -1,13 +1,13 @@
 //referenced this: https://github.com/mission-peace/interview/blob/master/src/com/interview/array/LongestIncreasingSubSequenceOlogNMethod.java
-const findNextHighest = (sequence, subsequenceTails, element)=>{
+const findNextHighest = (sequence, tails, element)=>{
     let low = 0,
-        high = subsequenceTails.length;
+        high = tails.length;
 
     while(high >= low){
         let mid = Math.floor((high + low) / 2);
-        if(element > sequence[subsequenceTails[mid]] && element <= sequence[subsequenceTails[mid+1]] ){
+        if(element > sequence[tails[mid]] && element <= sequence[tails[mid+1]] ){
             return mid+1;
-        } else if (element > sequence[subsequenceTails[mid]]){
+        } else if (element > sequence[tails[mid]]){
             low = mid + 1;
         }else{
             high = mid - 1;
@@ -18,7 +18,6 @@ const findNextHighest = (sequence, subsequenceTails, element)=>{
 
 
 const unfoldLongestSubsequence = (sequence, tails, trace)=>{
-
     let longestSequence = [];
     let nextElementIndex = tails[tails.length-1];
     while(nextElementIndex != null){
@@ -33,20 +32,18 @@ export default (sequence)=> {
 
     let trace = Array.from(Array(sequence.length), () => null);
 
-
     let tailIndices = sequence.reduce((tailIndices, element, index)=>{
         if(tailIndices.length == 0) { return [0]; }
 
         let low = sequence[tailIndices[0]],
             high = sequence[tailIndices[tailIndices.length-1]];
 
-        if(element > high){
+        if (element > high) {
             trace[index] = tailIndices[tailIndices.length-1];
             tailIndices.push(index);
-        }
-        else if (element < low){
+        } else if (element < low) {
             tailIndices[0] = index;
-        }else{
+        } else {
             let indexToBump = findNextHighest(sequence, tailIndices, element);
             tailIndices[indexToBump] = index;
             trace[tailIndices[indexToBump]] = tailIndices[indexToBump-1];

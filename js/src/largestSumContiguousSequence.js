16602bedf249f40cc734e3669a0e445cce41fcb2
Keith Gargano <keith.gargano@gmail.com>
2016-09-10 10:34:52 -0400

    LCS further edge cases accounted for

--- js/src/largestSumContiguousSequence.js
+++ js/src/largestSumContiguousSequence.js
@@ -3,24 +3,27 @@ export default (sequence)=> {
     let max = sum;
     let maxSequence = [sequence[0]];
 
-    let result = sequence.slice(1).reduce( (current, element) => {
+    return sequence.slice(1).reduce( (current, element) => {
         let { subsequence, max, sum, maxSequence } = current;
         sum = Math.max(sum, 0);
 
-        if( (sum + element) > 0){
+        if(sum <= 0 && element > sum){
+            sum = 0;
+            subsequence = [];
+        }
+
+        if( (sum + element) >= 0){
             sum = sum + element;
             subsequence.push(element);
-        } else if(sum + element <= 0){
+        } else if(sum + element < 0){
             sum = element;
             subsequence = [element];
         }
 
         if(sum > max) {
             max = sum;
-            maxSequence = subsequence;
+            maxSequence = subsequence.slice(0);
         }
         return { subsequence, max, sum, maxSequence };
     }, {sum, max, maxSequence, subsequence:maxSequence});
-
-    return result;
 };
--- js/src/largestSumContiguousSequence.spec.js
+++ js/src/largestSumContiguousSequence.spec.js
@@ -7,6 +7,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
         it('it should return that element as the max', () => {
             actual.max.should.equal(1);
         });
+        it('it should return a subsequence [1]', ()=>{
+            actual.maxSequence.should.deep.equal([1]);
+        });
     });
 
     describe('when running a sequence [1, 2]', () => {
@@ -15,6 +18,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
         it('it should return 3 as the max', () => {
             actual.max.should.equal(3);
         });
+        it('it should return a subsequence [1,2]', ()=>{
+            actual.maxSequence.should.deep.equal([1,2]);
+        });
     });
 
     describe('when running a sequence [-1, -10, 10, 2]', () => {
@@ -22,6 +28,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
             actual = largestSumContiguousSequence(sequence);
         it('it should return 12 as the max', () => {
             actual.max.should.equal(12);
+        });
+        it('it should return a subsequence [10,2]', ()=>{
+            actual.maxSequence.should.deep.equal([10,2]);
         });
     });
 
@@ -39,6 +48,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
         it('it should return 15 as the max', () => {
             actual.max.should.equal(15);
         });
+        it('it should return a subsequence [14, 1]', ()=>{
+            actual.maxSequence.should.deep.equal([14, 1]);
+        });
     });
 
     describe('when running a sequence [-1, -10, -3, -5]', () => {
@@ -46,6 +58,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
             actual = largestSumContiguousSequence(sequence);
         it('it should return -1 as the max', () => {
             actual.max.should.equal(-1);
+        });
+        it('it should return a subsequence [-1]', ()=>{
+            actual.maxSequence.should.deep.equal([-1]);
         });
     });
 
@@ -56,6 +71,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
         it('it should return -1 as the max', () => {
             actual.max.should.equal(-1);
         });
+        it('it should return a subsequence [-1]', ()=>{
+            actual.maxSequence.should.deep.equal([-1]);
+        });
     });
 
     describe('when running a sequence [-12, -10, -3, -5]', () => {
@@ -63,6 +81,9 @@ describe.only('largest sum contiguous sequence tests', ()=>{
             actual = largestSumContiguousSequence(sequence);
         it('it should return -3 as the max', () => {
             actual.max.should.equal(-3);
+        });
+        it('it should return a subsequence [-3]', ()=>{
+            actual.maxSequence.should.deep.equal([-3]);
         });
     });
 
@@ -72,6 +93,64 @@ describe.only('largest sum contiguous sequence tests', ()=>{
             actual = largestSumContiguousSequence(sequence);
         it('it should return 13 as the max', () => {
             actual.max.should.equal(13);
+        });
+        it('it should return a subsequence [8, 4, -1, 2]', ()=>{
+            actual.maxSequence.should.deep.equal([8, 4, -1, 2]);
+        });
+    });
+
+    describe('when running a sequence [8, 4, -3, 2]', () => {
+        let sequence = [8, 4, -3, 2],
+            actual = largestSumContiguousSequence(sequence);
+        it('it should return 12 as the max', () => {
+            actual.max.should.equal(12);
+        });
+        it('it should return a subsequence [8, 4]', ()=>{
+            actual.maxSequence.should.deep.equal([8, 4]);
+        });
+    });
+
+    describe('when running a sequence [8, 4, -13, 13]', () => {
+        let sequence = [8, 4, -13, 13],
+            actual = largestSumContiguousSequence(sequence);
+        it('it should return 13 as the max', () => {
+            actual.max.should.equal(13);
+        });
+        it('it should return a subsequence [13]', ()=>{
+            actual.maxSequence.should.deep.equal([13]);
+        });
+    });
+
+    describe('when running a sequence [8, 4, -2, 2]', () => {
+        let sequence = [8, 4, -2, 2],
+            actual = largestSumContiguousSequence(sequence);
+        it('it should return 12 as the max', () => {
+            actual.max.should.equal(12);
+        });
+        it('it should return a subsequence [8, 4]', ()=>{
+            actual.maxSequence.should.deep.equal([8, 4]);
+        });
+    });
+
+    describe('when running a sequence [8, 4, -2, 3]', () => {
+        let sequence = [8, 4, -2, 3],
+            actual = largestSumContiguousSequence(sequence);
+        it('it should return 13 as the max', () => {
+            actual.max.should.equal(13);
+        });
+        it('it should return a subsequence [8, 4, -2, 3]', ()=>{
+            actual.maxSequence.should.deep.equal([8, 4, -2, 3]);
+        });
+    });
+
+    describe('when running a sequence [20, 40, -5, 10]', () => {
+        let sequence = [20, 40, -5, 10],
+            actual = largestSumContiguousSequence(sequence);
+        it('it should return 65 as the max', () => {
+            actual.max.should.equal(65);
+        });
+        it('it should return a subsequence [20, 40, -5, 10]', ()=>{
+            actual.maxSequence.should.deep.equal([20, 40, -5, 10]);
         });
     });
 });

6eb38689047302bad51dc80f6dd5bffe933d0a72
Keith Gargano <keith.gargano@gmail.com>
2016-09-12 12:38:47 -0400

    bridges LIS algorithm mostly works, edge cases to fix

--- js/src/bridgeCities.js
+++ js/src/bridgeCities.js
@@ -5,9 +5,10 @@ const findNextHighest = (bridges, tails, bridge)=>{
     while(high >= low){
         let mid = Math.floor((high+low)/2);
 
-        if(bridge.north > bridges[tails[mid]].north && bridge.north <= bridges[tails[mid+1]].north){
+        console.log('sorting', bridges, high, mid, low);
+        if(bridge.north > bridges[tails[mid]].north && bridge.south <= bridges[tails[mid+1]].north){
             return mid+1;
-        }else if(bridge.north > bridges[tails[mid]]){
+        }else if(bridge.north > bridges[tails[mid]].north){
             low = mid+1;
         }else{
             high = mid-1;
@@ -21,8 +22,7 @@ export default (cities)=>{
     let n = cities[0].split(''),
         s = cities[1].split(''),
         nLocations = n.reduce((map, city, index)=>  { map[city] = index; return map; } , {}),
-        bridges = s.map((city, index)=> ({ south: index, north: nLocations[city], city }));
-
+        bridges = s.map((city, index)=> ({ south: index, north: nLocations[city], city })).filter(b=> b.north);
 
     let sequenceTrace = Array.from(Array(bridges.length), (b)=>null);
 
@@ -38,23 +38,21 @@ export default (cities)=>{
         }else if(bridge.north < low){
             tailIndices[0] = index;
         }else{
+            
             let nextHighestIndex = findNextHighest(bridges, tailIndices, bridge);
-
+            tailIndices[nextHighestIndex] = index;
+            sequenceTrace[index] = tailIndices[nextHighestIndex-1];
         }
 
         return tailIndices;
     }, []);
 
-
-    console.log("START");
-    console.log("tails", tailIndices);
     let result = [];
     let b = tailIndices[tailIndices.length-1];
     while(b != null){
         result.push(bridges[b].city);
         b = sequenceTrace[b];
     }
-    console.log('result', result, result.join(''))
 
     return result.reverse().join('');
 }
--- js/src/bridgeCities.spec.js
+++ js/src/bridgeCities.spec.js
@@ -37,57 +37,52 @@ describe.only('city bridging', ()=>{
             });
         });
 
-        describe('only the center connects', ()=>{
+
+        describe('abc, bca', ()=>{
+            let bridgedCities = bridgeCities(['abc', 'bca']);
+            it('it should match bc', ()=> {
+                bridgedCities.should.equal('bc');
+            });
+        });
+
+        describe('when all the cities uniquely connect, it matches the last one \n abc \n cba', ()=>{
             let bridgedCities = bridgeCities(['abc', 'cba']);
             it('it should match for every city pair', ()=> {
                 bridgedCities.should.equal('b');
             });
         });
 
-        //                [a, b, c, d, e, f, g]
-        //                [c, a, e, g, f, b, d]
-        describe('such as [a, b, c, d, e, f, g] and [c, a, e, g, f, b, d] ', ()=>{
+        //                abcdefg
+        //                caegfbd
+        describe('such as \nabcdefg and \ncaegfbd ', ()=>{
             let bridgedCities = bridgeCities(['abcdefg', 'caegfbd']);
-            it('it should pair aef, cef, ceg ', ()=> {
-                bridgedCities.should.deep.equal(['aef', 'cef', 'ceg']);
+            it('it should suggest cef', ()=> {
+                bridgedCities.should.equal('cef');
             });
         });
 
-        //                [a, c, d, e, f, b, g]
-        //                [c, d, e, g, f, b, a]
-        describe('such as [a, c, d, e, f, b, g] and [c, a, e, g, f, b, d] ', ()=>{
-            let bridgedCities = bridgeCities(['abcdefg', 'caegfbd']);
-            it('it should pair bcdef', ()=> {
-                bridgedCities.should.equal('bcdef');
-            });
-        });
-
-
-        //a, 0, 6
-        //c, 6, 1
-        //d, 6, 1
-        //e, 6, 1
-        //f, 7, 0
-        //b, 7, 0
-        //g, 5, 3
-        //                [a, c, d, e, f, b, g]
-        //                [c, d, e, g, f, b, a]
-        //at f, nothing past f on the north or south bank can go west of f.. or east of f...
-        //at b, nothing past b on the north or south bank can go west of b or east of b
-        //at c, nothing before c on the north bank can go east of c... hm..
-        describe('such as [a, c, d, e, f, b, g] and [c, a, e, g, f, b, d] ', ()=>{
+        //                caegfbd
+        //                abcdefg
+        describe('such as \ncaegfbd and \nabcdefg ', ()=>{
             let bridgedCities = bridgeCities(['caegfbd', 'abcdefg']);
-            it('it should pair bcdef', ()=> {
-                bridgedCities.should.equal('bcdef');
+            it('it should pair aef', ()=> {
+                bridgedCities.should.equal('aef');
+            });
+        });
+
+        describe('such as acdefbg and cdegfba ', ()=>{
+            let bridgedCities = bridgeCities(['acdefbg', 'cdegfba']);
+            it('it should pair cdefb', ()=> {
+                bridgedCities.should.equal('cdefb');
             });
         });
 
 
         //                [b, a, c, g, f, e, d]
         //                [c, a, e, g, f, b, d]
-        describe('such as [a, b, c, d, e, f, g] and [c, a, e, g, f, b, d] ', ()=>{
-            let bridgedCities = bridgeCities(['bacgfed', 'caegfd']);
-            it('it should suggest egfd and agfd', ()=> {
+        describe('such as bacgfed and caegfbd', ()=>{
+            let bridgedCities = bridgeCities(['bacgfed', 'caegfbd']);
+            it('it should suggest agfd', ()=> {
                 bridgedCities.should.equal('agfd');
             });
         });
